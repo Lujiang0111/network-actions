2979251a094500a9f76c1ce0cf846f3189e52f2a
lujiang <lujiang0111@gmail.com>
2023-11-25 02:15:04 +0800

    CNIP添加组播地址 添加北京联通组播地址

--- cn_address_list/action.sh
+++ cn_address_list/action.sh
@@ -4,12 +4,17 @@
 curl -s https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/geolite2_country/country_cn.netset \
 | sed -e '/^#/d'\
 | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP/g' \
-| sed -e '1i/ip firewall address-list' -e '1iremove [/ip firewall address-list find list=CNIP]' -e '1iadd address=10.0.0.0/8 list=CNIP comment=private-network' -e '1iadd address=172.16.0.0/12 list=CNIP comment=private-network' -e '1iadd address=192.168.0.0/16 list=CNIP comment=private-network' \
+| sed -e '1i/ip firewall address-list' -e '1iremove [/ip firewall address-list find list=CNIP]' -e '1iadd address=10.0.0.0/8 list=CNIP comment=private-network' -e '1iadd address=172.16.0.0/12 list=CNIP comment=private-network' -e '1iadd address=192.168.0.0/16 list=CNIP comment=private-network' -e '1iadd address=224.0.0.0/4 list=CNIP comment=private-network' \
 > geolite2.rsc
 
 # update from chnroutes2
 curl -s https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt \
 | sed -e '/^#/d'\
 | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP/g' \
-| sed -e '1i/ip firewall address-list' -e '1iremove [/ip firewall address-list find list=CNIP]' -e '1iadd address=10.0.0.0/8 list=CNIP comment=private-network' -e '1iadd address=172.16.0.0/12 list=CNIP comment=private-network' -e '1iadd address=192.168.0.0/16 list=CNIP comment=private-network' \
+| sed -e '1i/ip firewall address-list' -e '1iremove [/ip firewall address-list find list=CNIP]' -e '1iadd address=10.0.0.0/8 list=CNIP comment=private-network' -e '1iadd address=172.16.0.0/12 list=CNIP comment=private-network' -e '1iadd address=192.168.0.0/16 list=CNIP comment=private-network' -e '1iadd address=224.0.0.0/4 list=CNIP comment=private-network' \
 > chnroutes2.rsc
+
+# update IPTV address from Beijing-IPTV
+curl -s https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Unicom.m3u \
+| sed -e 's/192.168.123.1/192.168.8.5/g' \
+> Beijing_Unicom.m3u
