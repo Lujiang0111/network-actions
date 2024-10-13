cd0303ecef97fb53a2ada5c47f36c60d3006dabd
Lujiang <lujiang0111@gmail.com>
2024-10-13 16:45:04 +0800

    旁路由IP更改为8.11

--- list/iptv/action.sh
+++ list/iptv/action.sh
@@ -2,5 +2,5 @@
 
 # update IPTV address from Beijing-IPTV
 curl -s https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Unicom.m3u |
-    sed -e 's/192.168.123.1/192.168.8.5/g' \
+    sed -e 's/192.168.123.1/192.168.8.11/g' \
         >beijing_unicom.m3u
