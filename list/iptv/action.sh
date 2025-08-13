2a068ab7cb05ba8672925d31d055fc22eee8996f
Lujiang <lujiang0111@gmail.com>
2025-08-13 15:34:25 +0800

    添加组播形式

--- list/iptv/action.sh
+++ list/iptv/action.sh
@@ -2,5 +2,8 @@
 
 # update IPTV address from Beijing-IPTV
 curl -s https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Unicom.m3u |
-    sed -e 's/192.168.123.1/192.168.8.11/g' \
+    sed -e 's/192.168.123.1/172.28.8.21/g' \
         >beijing_unicom.m3u
+
+curl -s https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/refs/heads/master/IPTV-Unicom-Multicast.m3u \
+    >beijing_unicom_multicast.m3u
